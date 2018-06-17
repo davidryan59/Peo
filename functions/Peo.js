1637d750542eebf77712cd8639d3d129a46bf207
David Ryan <davidryan1998@hotmail.com>
2018-06-17 21:11:56 +0700

    toString moved to API

--- functions/Peo.js
+++ functions/Peo.js
@@ -1,28 +1,25 @@
 var initialise = require('./initialisers/initialise')
 
-var setFraction = require('./setters/setFraction')
-
 var mult = require('./api/mult')
 var getText = require('./api/getText')
+var toString = require('./api/toString')
 var getFraction = require('./api/getFraction')
 var getPrimeExp = require('./api/getPrimeExp')
 var getPrimeExps = require('./api/getPrimeExps')
 var checkPrimeExps = require('./api/checkPrimeExps')
 
-// Here's the Peo class
+// Peo class constructor
 function Peo() {
-
-  // Set up structure
-  this.p = {}              // This is the main data store. Its going to be set with primes and exponents
-  this.fr = null           // Going to set a Fraction in here later (small only)
-  this.txt = ""            // Going to set some descriptive text here later
-
-  // Initialise from arguments
+  // Control of Peo is here
+  this.p = {}
+  // Cached info to speed up Peo is here. Each value should start off falsey.
+  this.fr = null
+  this.txt = ""
+  // Initialise from various arguments
   initialise(this, arguments)
-
 }
 
-// Do the copy function here, it requires the constructor
+// Do the copy function here since it requires the constructor
 Peo.prototype.copy = function() {
   // Start with a new Peo representing 1/1
   var copyPeo = new Peo()
@@ -33,13 +30,13 @@ Peo.prototype.copy = function() {
   return copyPeo
 }
 
-// From API folder
+// Setup the public API
 Peo.prototype.mult = mult
 Peo.prototype.getText = getText
+Peo.prototype.toString = toString
 Peo.prototype.getFraction = getFraction
 Peo.prototype.getPrimeExp = getPrimeExp
 Peo.prototype.getPrimeExps = getPrimeExps
 Peo.prototype.checkPrimeExps = checkPrimeExps
-Peo.prototype.toString = function() {return this.getText()}
 
 module.exports = Peo
